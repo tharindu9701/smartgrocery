623084f4b7a22297e0b174ed91f0618e589c5dc3
tharindu9701 <priyanethgamage@gmail.com>
2024-07-04 10:45:47 +0530

    Add files via upload

---
+++ product.model.ts
@@ -0,0 +1,12 @@
+export interface Product{
+    id : string;
+    productName : string;
+    description  : string;
+    catagory : string;
+    brand : string;
+    expiredDate : string;
+    manufacturedDate : string;
+    createdDate : string;
+    quantity : number;
+    unitPrice : number;
+}
